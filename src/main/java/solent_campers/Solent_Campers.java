3e389c901a2de6e02a6f5cabe967a83b186bc970
asus <asus@LAPTOP-SOGON9PN>
2021-12-02 23:16:19 +0530

    Form Class Added+Modified

---
+++ src/main/java/solent_campers/Solent_Campers.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package solent_campers;
+
+/**
+ *
+ * @author kaur
+ */
+public class Solent_Campers {
+
+    /**
+     * @param args the command line arguments
+     */   static int vanid=0;
+      static int campid=0;
+       static int carid=0;
+    public static void main(String[] args) {
+        // TODO code application logic here
+      MainForm mf=new MainForm();
+        mf.setVisible(true);
+    }
+
+}
